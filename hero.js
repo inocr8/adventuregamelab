6638c866ced8fa70f9d2227fbbd08ffa9740f4e9
Euan Walls <apps@inocr8.co>
2016-01-27 15:21:18 +0000

    addition of the belly array

--- hero.js
+++ hero.js
@@ -4,6 +4,7 @@ var Hero = function(name, health, favouriteFood){
   this.name = name;
   this.health = health;
   this.favouriteFood = favouriteFood;
+  this.belly = [];
   this.talk = function(){
     // console.log("I am a hero, my name is " + this.name);
     return("I am a hero, my name is " + this.name);
